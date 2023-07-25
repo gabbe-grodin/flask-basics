d667dd93c39302ae7980fef2db2e70a0def923a7
Gabbe Grodin, MFA <gabbe.grodin@gmail.com>
2023-07-24 19:50:07 -0500

    new assignments added plus debugging previous work

--- hello-flask/server.py
+++ hello-flask/server.py
@@ -6,30 +6,43 @@ def hello_world():
     # return 'Hello World!'
     return render_template('index.html')
 
-def index():
-    return render_template("index.html", phrase="hello", times=5)
+# def index():
+#     return render_template("index.html", phrase="hello", times=5)
 
-@app.route('/success')
-def success():
-    return "success"
+# @app.route('/success')
+# def success():
+#     return "success"
 
 
-@app.route('/hello/<name>')
-def hello(name):
-    print(name)
-    return "Hello, " + name
+# @app.route('/hello/<name>')
+# def hello(name):
+#     print(name)
+#     return "Hello, " + name
 
-@app.route('/users/<username>/<id>')
-def show_user_profile(username, id):
-    print(username)
-    print(id)
-    return "username: " + username + ", id: " + id
+# @app.route('/users/<username>/<id>')
+# def show_user_profile(username, id):
+#     print(username)
+#     print(id)
+#     return "username: " + username + ", id: " + id
 
-@app.route('/hello/<name>/<int:num>') 
-def hello_again(name, num):
-    # return f"Hello, {name * num}"
-    return render_template("hello.html",name=name,num=num)
+# @app.route('/hello/<name>/<int:num>') 
+# def hello_again(name, num):
+#     # return f"Hello, {name * num}"
+#     return render_template("hello.html",name=name,num=num)
+
+@app.route('/lists')
+def render_lists():
+    # Soon enough, we'll get data from a database, but for now, we're hard coding data
+    student_info = [
+        {'name' : 'Michael', 'age' : 35},
+        {'name' : 'John', 'age' : 30 },
+        {'name' : 'Mark', 'age' : 25},
+        {'name' : 'KB', 'age' : 27}
+    ]
+    return render_template("lists.html", random_numbers = [3,1,5], students = student_info)
+
+
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="localhost", port=8000)
 
